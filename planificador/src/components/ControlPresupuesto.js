77c822cea1529e27666e86cce371be4079a012e3
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-10 18:43:50 -0500

    Created global styles for the container. Add image and styled it

--- planificador/src/components/ControlPresupuesto.js
+++ planificador/src/components/ControlPresupuesto.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
+
+import globalStyles from '../styles';
 
 function ControlPresupuesto() {
-  return <Text>Desde Contro lPresupuesto</Text>;
+  return (
+    <View style={styles.contenedor}>
+      <View style={styles.centrarGrafica}>
+        <Image source={require('../img/grafico.jpg')} style={styles.imagen} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  contenedor: {
+    ...globalStyles.contenedor,
+  },
+  centrarGrafica: {
+    alignItems: 'center',
+  },
+  imagen: {
+    width: 250,
+    height: 250,
+  },
+});
 
 export default ControlPresupuesto;
--- planificador/src/components/NuevoPresupuesto.js
+++ planificador/src/components/NuevoPresupuesto.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native';
 
+import globalStyles from '../styles';
+
 function NuevoPresupuesto(props) {
   const {handleNuevoPresupuesto} = props;
 
@@ -27,20 +29,7 @@ function NuevoPresupuesto(props) {
 
 const styles = StyleSheet.create({
   contenedor: {
-    backgroundColor: '#fff',
-    marginHorizontal: 10,
-    borderRadius: 10,
-    paddingVertical: 40,
-    paddingHorizontal: 20,
-    transform: [{translateY: 50}],
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 2.62,
-    elevation: 4,
+    ...globalStyles.contenedor,
   },
   label: {
     textAlign: 'center',
