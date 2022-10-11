496daaa99846c7cc284f30aeb9ae2d7fc49bfc9f
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-11 11:37:03 -0500

    Add style to Gasto component

--- planificador/src/components/Gasto.js
+++ planificador/src/components/Gasto.js
@@ -17,14 +17,19 @@ const Gasto = ({gasto}) => {
   const {nombre, categoria, cantidad, id} = gasto;
   return (
     <View style={styles.contenedor}>
-      <View>
-        <Image source={diccionarioCategorias[categoria]} />
-        <View>
-          <Text>{categoria}</Text>
-          <Text>{nombre}</Text>
+      <View style={styles.contenido}>
+        <View style={styles.contenedorImagen}>
+          <Image
+            source={diccionarioCategorias[categoria]}
+            style={styles.imagen}
+          />
+          <View style={styles.contenedorTexto}>
+            <Text style={styles.categoria}>{categoria}</Text>
+            <Text style={styles.nombre}>{nombre}</Text>
+          </View>
         </View>
+        <Text style={styles.cantidad}>{formatearCantidad(cantidad)}</Text>
       </View>
-      <Text>{formatearCantidad(cantidad)}</Text>
     </View>
   );
 };
@@ -33,6 +38,40 @@ const styles = StyleSheet.create({
   contenedor: {
     ...globalStyles.contenedor,
     marginBottom: 20,
+  },
+  contenido: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  contenedorImagen: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+  imagen: {
+    width: 80,
+    height: 80,
+    marginRight: 20,
+  },
+  contenedorTexto: {
+    flex: 1,
+  },
+  categoria: {
+    color: '#94A3b8',
+    fontSize: 16,
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    marginBottom: 5,
+  },
+  nombre: {
+    fontSize: 22,
+    color: '#64748B',
+    marginBottom: 5,
+  },
+  cantidad: {
+    fontSize: 20,
+    fontWeight: '700',
   },
 });
 
