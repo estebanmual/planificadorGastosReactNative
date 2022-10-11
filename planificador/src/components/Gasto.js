99e54c3ba20a5d88f827348aae13bd78dfb84022
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-11 11:25:36 -0500

    Add image to gastos component

--- planificador/src/components/Gasto.js
+++ planificador/src/components/Gasto.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
 import globalStyles from '../styles';
+import {formatearCantidad} from '../helpers';
+
+const diccionarioCategorias = {
+  ahorro: require('../img/icono_ahorro.png'),
+  comida: require('../img/icono_comida.png'),
+  casa: require('../img/icono_casa.png'),
+  gastos: require('../img/icono_gastos.png'),
+  ocio: require('../img/icono_ocio.png'),
+  salud: require('../img/icono_salud.png'),
+  suscripciones: require('../img/icono_suscripciones.png'),
+};
 
 const Gasto = ({gasto}) => {
   const {nombre, categoria, cantidad, id} = gasto;
   return (
     <View style={styles.contenedor}>
-      <Text>{nombre}</Text>
+      <View>
+        <Image source={diccionarioCategorias[categoria]} />
+        <View>
+          <Text>{categoria}</Text>
+          <Text>{nombre}</Text>
+        </View>
+      </View>
+      <Text>{formatearCantidad(cantidad)}</Text>
     </View>
   );
 };
