1159a22962ec5d19f8fb6b1f710fc79bcf2305e2
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-10 19:54:35 -0500

    Added picker for the form

--- planificador/src/components/FormularioGasto.js
+++ planificador/src/components/FormularioGasto.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const FormularioGasto = () => {
   return (
     <SafeAreaView>
-      <Text>Formulario Gasto</Text>
+      <View>
+        <Pressable>
+          <Text>Cancelar</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text>Nuevo Gasto</Text>
+        <View>
+          <Text>Nombre Gasto</Text>
+          <TextInput placeholder="Nombre del gasto. ej. Transporte" />
+        </View>
+        <View>
+          <Text>Cantidad Gasto</Text>
+          <TextInput
+            placeholder="Cantidad del gasto. ej. 300"
+            keyboardType="numeric"
+          />
+        </View>
+        <View>
+          <Text>Categoría Gasto</Text>
+          <Picker>
+            <Picker.Item label="--Seleccione--" value="" />
+            <Picker.Item label="Ahorro" value="ahorro" />
+            <Picker.Item label="Comida" value="comida" />
+            <Picker.Item label="Casa" value="casa" />
+            <Picker.Item label="Gastos Varios" value="gastos" />
+            <Picker.Item label="Ocio" value="ocio" />
+            <Picker.Item label="Salud" value="salud" />
+            <Picker.Item label="Suscripciones" value="suscripciones" />
+          </Picker>
+        </View>
+
+        <Pressable>
+          <Text>Agregar Gasto</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
